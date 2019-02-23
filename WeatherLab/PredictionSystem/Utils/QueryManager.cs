6119c435dc1948234984fd40755013b66866e949
Amine <hm_raiah@esi.dz>
2019-02-23 23:13:26 +0100

    Je viens de pusher la classe QueryManager

--- WeatherLab/PredictionSystem/Utils/QueryManager.cs
+++ WeatherLab/PredictionSystem/Utils/QueryManager.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace WeatherLab.PredictionSystem.Utils
 {
-    class QueryManager
+    public class QueryManager       //Cette classe contient une liste d'inputs selon le choix du User + Le delait de prediction + La date du jour 
     {
+        public List<ParamValue> Liste = new List<ParamValue>();
+        public double delai { get; set; }
+        public DateTime date = new DateTime();
+
+        public void add(ParamValue newParamValue)       //CEtte méthode ajoute un nouveau tuplet ParamValue a la liste
+        {
+            this.Liste.Add(newParamValue);
+        }
+
+        public int length()         //REtourne la taille de la liste
+        {
+            return Liste.Count;
+        }
+
+    }
+
+    public class ParamValue        //Cette Classe va contenir Le tuplet <Param,Value> 
+    {
+        public Param Parametre { get; set; }
+        public int Value { get; set; }
+    }
+
+    public enum Param       //Ce type contient Les parametres qui se trouve dans le Dataset et que l'utilisateur pourra Choisir Par la suite
+    {
+        Temp,
+        Pression,
+        Humidite,
+        DirectionVent,
+        VitesseVent,
+        PourcentageNuage,
+        DistanceVisibilite,
+        EtatSol
     }
 }
