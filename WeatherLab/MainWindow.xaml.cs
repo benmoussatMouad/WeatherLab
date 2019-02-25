920cfe9adf52c586f7c9eb6915a722caa754f918
Mouad Benmoussat <hm_benmoussat@esi.dz>
2019-02-25 18:03:45 +0100

    Added untracked files that i forgot in the previous commit

---
+++ WeatherLab/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace WeatherLab
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
