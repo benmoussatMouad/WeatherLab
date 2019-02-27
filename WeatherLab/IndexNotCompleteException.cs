07c8bb641fd3552662b59b1ca24659c47dedc7a5
nabil-otsmane <hn_otsmane@esi.dz>
2019-02-27 21:58:01 +0100

    major changes

---
+++ WeatherLab/IndexNotCompleteException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherLab.Data
+{
+    class IndexNotCompleteException : Exception
+    {
+        public IndexNotCompleteException()
+        {
+
+        }
+
+        public IndexNotCompleteException(string msg) : base(msg)
+        {
+
+        }
+
+    }
+}
